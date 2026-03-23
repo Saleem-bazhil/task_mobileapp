dec9660eec8ce7ca0e90bca7905c026b079f86e2
Saleem-bazhil <ab588538@gmail.com>
2026-03-23 11:14:20 +0530

    add the componets of the chat

---
+++ src/components/ChatScreenComponents/ConversationSidebar.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ConversationSidebar = () => {
+  return (
+    <View>
+      <Text>ConversationSidebar</Text>
+    </View>
+  )
+}
+
+export default ConversationSidebar
